96dcbb7c00bcf35ddf9531e77ca81f653a5b5b05
justingeorgebinu7 <justingeorgebinu@gmail.com>
2021-11-16 15:06:41 +0530

    Revenue item edit  product/quantity

---
+++ ViewController/src/Beans/RevenueItemedit.java
@@ -0,0 +1,79 @@
+package Beans;
+
+import java.io.Serializable;
+
+import javax.faces.event.ValueChangeEvent;
+import java.math.BigDecimal;
+
+import javax.faces.event.ValueChangeEvent;
+import oracle.binding.BindingContainer;
+
+import oracle.binding.OperationBinding;
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCIteratorBinding;
+
+import oracle.jbo.Row;
+import oracle.jbo.RowSetIterator;
+import java.math.*;
+public class RevenueItemedit implements Serializable {
+    public RevenueItemedit() {
+    }
+
+    public BindingContainer getBindingsCont() {
+
+          return BindingContext.getCurrent().getCurrentBindingsEntry();
+
+      }
+
+
+
+      /**
+
+       * Generic Method to execute operation
+
+       * */
+
+      public OperationBinding executeOperation(String operation) {
+
+          OperationBinding createParam = getBindingsCont().getOperationBinding(operation);
+
+          return createParam;
+
+
+
+      }
+    public void productSelect(ValueChangeEvent valueChangeEvent) {
+        Object pid= valueChangeEvent.getNewValue();
+        
+         OperationBinding ob = executeOperation("estimatedPrice");
+         ob.getParamsMap().put("pidStr", pid.toString());
+         ob.execute();
+         
+    }
+
+    public void quantity(ValueChangeEvent valueChangeEvent) {
+        Object quant= valueChangeEvent.getNewValue();
+        DCIteratorBinding it = (DCIteratorBinding)BindingContext.getCurrent().getCurrentBindingsEntry().get("RevenueitemView2Iterator");   
+        RowSetIterator rsIter = it .getRowSetIterator();
+        Row rowObj = rsIter .getCurrentRow();
+        Object price=rowObj.getAttribute("Estimatedprice");
+        if(price==null) {
+            return;
+        }
+        int q=Integer.parseInt(quant.toString());
+        int p=Integer.parseInt(price.toString());
+        int rev=p*q;
+        
+        Object revenue=(Integer)rev;
+        //System.out.println(rev);
+        double bestcaserev= 1.1*rev;
+        //System.out.println(bestcaserev);
+        double worstcaserev=0.9*rev;
+        Object best=bestcaserev;
+        Object worst=worstcaserev;
+        rowObj.setAttribute("Revenue",revenue);
+        rowObj.setAttribute("Bestcaserevenue",best); 
+        rowObj.setAttribute("Worstcaserevenue",worst);
+    }
+}
